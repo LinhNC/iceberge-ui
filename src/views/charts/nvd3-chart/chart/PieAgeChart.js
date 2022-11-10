e3cb8cb97d6ed1d8ce45f61fdff23a73ae755104
Nguyen Chi Linh <LinhNC@batdongsan.com.vn>
2022-11-10 09:57:09 +0700

    update config pie age chart

--- src/views/charts/nvd3-chart/chart/PieAgeChart.js
+++ src/views/charts/nvd3-chart/chart/PieAgeChart.js
@@ -2,12 +2,12 @@ import React from 'react';
 import NVD3Chart from 'react-nvd3';
 
 const datum = [
-    { key: '18 - 24', y: 29, color: '#ff8a65' },
-    { key: '25 - 34', y: 0, color: '#f4c22b' },
-    { key: '35 - 44', y: 32, color: '#04a9f5' },
-    { key: '45 - 54', y: 196, color: '#3ebfea' },
-    { key: '55 - 64', y: 2, color: '#4F5467' },
-    { key: '65+', y: 98, color: '#1de9b6' },
+    { key: '18 - 24', y: 19, color: '#ff8a65' },
+    { key: '25 - 34', y: 30, color: '#f4c22b' },
+    { key: '35 - 44', y: 72, color: '#04a9f5' },
+    { key: '45 - 54', y: 46, color: '#3ebfea' },
+    { key: '55 - 64', y: 25, color: '#4F5467' },
+    { key: '65+', y: 28, color: '#1de9b6' },
 ];
 
 const PieAgeChart = () => {
